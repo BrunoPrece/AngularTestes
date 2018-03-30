f0b0ac95824cf4a3db9ab3d7e10ddaea701a46d1
Nicolas Aoki <nicolasaoki@alunos.utfpr.edu.br>
2018-03-29 21:37:03 -0300

    primeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GaleraComponent } from './galera/galera.component';
+import { FaleconoscoComponent } from './faleconosco/faleconosco.component';
 
+const appRoutes:Routes = [
+  { path:'', component: HomeComponent },
+  { path:'home', component: HomeComponent },
+  { path:'faleconosco', component: FaleconoscoComponent },
+  { path:'galera', component: GaleraComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GaleraComponent,
+    FaleconoscoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
